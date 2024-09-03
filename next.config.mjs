dada861a7d39ccd6fa9ab89804a6c7ad683cc737
shahdivyank <divyank.shah.2016@gmail.com>
2024-09-02 19:44:31 -0700

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "",
+};
 
-export default nextConfig;
+module.exports = nextConfig;
