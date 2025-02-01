e6f8699b8b152824e856fbb828c8c2f022199100
Swooshoo <eliotlee1415@gmail.com>
2025-01-31 22:22:04 -0800

    Added experiences, organizations, and teaching

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -1,73 +1,38 @@
-import Cisco from "@/public/logos/cisco.png";
-import Comcast from "@/public/logos/comcast.webp";
-import ZyberTech from "@/public/logos/zybertech.png";
+import Mobalytics from "@/public/logos/mobalytics.jpg"
+import Apple from "@/public/logos/apple.svg"
 import Layout from "@/components/global/layout";
 import { tile } from "@/types";
 
 const experiences: tile[] = [
   {
-    logo: ZyberTech,
-    group: "Mass Scale Infrastructure Group",
+    logo: Mobalytics,
+    group: "Gaming Market Research",
     location: "Remote California",
-    role: "Software Engineer II",
+    role: "Data Analytics Intern",
     start: "April 2024",
-    end: "Present",
-    alt: "ZyberTech Logo",
+    end: "June 2024",
+    alt: "Mobalytics Logo",
     description: [
-      "Architect Android based React Native Expo self-ordering kiosk app for Farmer Boys Kiosk Pilot in August 2024",
-      "Rewrote file based API cache to reduce Point of Sale (POS) requests by 80% with 24 hour cache retention policy",
-      "Integrate Worldpay TriPOS Payment API with custom Kotlin wrapper to provide P2PE PCI certified payments",
-      "Implement 18+ screens to handle member and guest user flows to personalize ordering experience",
+      "Conducted comprehensive market research and data analysis on two major e-sports titles, analyzing key performance indicators and in-game decision-making patterns",
+      "Generated actionable insights on player behavior, gameplay mechanics, and community dynamics to inform strategic decision-making and potential business expansion opportunities",
+      "Participated in weekly standups, presenting in a final capstone presentation towards the cohort and staff leads.",
     ],
   },
   {
-    logo: Cisco,
-    group: "Mass Scale Infrastructure Group",
-    location: "San Jose, California",
-    role: "Software Engineering Intern",
-    start: "July 2024",
-    end: "Present",
-    alt: "Cisco Logo",
-    description: [
-      "Construct FireX Auto Triaging system to infer failure reason on testlogs reducing developer intervention by 20%",
-      "Utilize Facebook AI Similarity Search (FAISS) to index FireX logs and semantically search across 3000+ documents",
-      "Embed testlogs with Hugging Face’s all-MiniLM-L6-v2 & integrate with LangChain to build FAISS IndexFlatL2",
-      "Undertake Next.js + Typescript + Shadcn labeling dashboard to display MongoDB data points via aggregation pipeline",
-      "Extend FireX testing plugins to automatically parse and push XML testcase logs to MongoDB",
-    ],
-  },
-  {
-    logo: Comcast,
-    group: "Smart Network Platform",
-    location: "Philadelphia, Pennsylvania",
-    role: "Software Engineering Intern",
-    start: "June 2023",
-    end: "September 2023",
-    alt: "Comcast Logo",
-    description: [
-      "Upgrade Foresight web app to use Node.js 18 from v12 to increase performance and reduce security vulnerabilities",
-      "Merge Express.js API with Foresight web app to reduce running containers by 50% & increase performance by 25%",
-      "Refine project name input data validation API to prevent name duplication and provide useful error messages",
-      "Enhance SNP Storybook webcomponents to resolve 10+ Mend alerts to maintain quarterly security compliance",
-      "Design and develop Slack based VM telemetry system with 4 panel Grafana dashboard pulling from InfluxDB",
-    ],
-  },
-  {
-    logo: Comcast,
-    group: "Next Generatation Area Network (NGAN)",
+    logo: Apple,
+    group: "Ad Astra Behavioral Analytics Service Group",
     location: "Remote California",
-    role: "Software Engineering Intern",
-    start: "June 2022",
-    end: "Feburary 2023",
-    alt: "Comcast Logo",
+    role: "iOS Swift Design Intern",
+    start: "March 2022",
+    end: "September 2022",
+    alt: "Apple Logo",
     description: [
-      "Fabricate 5+ Concourse pipelines to check linting, formatting, whitesourcing, testing",
-      "Compute event-triggered TTR to enhance accuracy for 1000+ daily alerts & visualize with React dashboard",
-      "Migrate & mock unit-test auto-remediation platform from Stackstorm to AWS Step Functions",
-      "Standardize 50+ Python auto-remediation scripts to fit security requirements and AWS Step Function compatibility",
-      "Write & automate 320+ unit tests via PyTest and Concourse to ensure 15+ APIs’ robustness with 95% code coverage",
+      "Fabricated app proposals alongside a 7+ intern team to meet client Ad Astra Behavior Analytic Services’ needs",
+      "Attended weekly standups with mentors and client for constructive feedback on prototypes",
+      "Presented our prototype in a final capstone presentation to our local client & group leads",
+      "Completed a complementary 180 hour online course for iOS Swift.",
     ],
-  },
+  }
 ];
 
 const Experiences = () => {
--- src/components/teaching.tsx
+++ src/components/teaching.tsx
@@ -1,75 +1,55 @@
 import UCR from "@/public/logos/ucr.webp";
-import CodeNinjas from "@/public/logos/codeninjas.png";
-import CaliforniaRobotics from "@/public/logos/california.png";
-import FirstLegoLeague from "@/public/logos/lego.png";
+import ACM from "@/public/logos/acm.webp"
+import Teach from "@/public/logos/teach.png"
 import Layout from "@/components/global/layout";
 import { tile } from "@/types";
 
 const teachings: tile[] = [
   {
-    logo: UCR,
-    group: "Computer Science Department",
-    location: "Riverside, California",
-    role: "Teaching Assistant",
-    start: "March 2024",
+    logo: Teach,
+    group: "Mathematics",
+    location: "Los Angeles, California",
+    role: "Private Tutor",
+    start: "June 2024",
     end: "Present",
+    alt: "Private Tutor",
+    description: [
+      "Currently tutoring 5 students in geometry, algebra, and precalculus, providing personalized coaching to strengthen problem-solving skills.",
+      "Helped students achieve A+ grades (top 10% percentile) in their math classes through targeted lessons and structured practice.",
+      "Designed interactive lessons incorporating real-world applications and visual aids, improving conceptual understanding and retention on a student-by-student basis.",
+      "Created custom worksheets, quizzes, and review sessions, leading to measurable improvements in test scores and confidence.",
+    ],
+  },
+  {
+    logo: ACM,
+    group: "ACM Data Science Workshop Series",
+    location: "Riverside, California",
+    role: "Tableau Workshop Lead",
+    start: "Aug 2023",
+    end: "Aug 2023",
     alt: "UCR Logo",
     description: [
-      "Work with 300+ students teaching lists, stacks, queues, trees, heaps, hashtables, graphs",
-      "Take programming and laboratory assignments whiteboard demos encouraging students to think critically about new problems and approaches",
-      "Hold weekly office hours to help students debug, review and understand course content",
-      "Instruct 16 x 1 hour quiz preparation/review sessions to prepare students for biweekly quizzes",
-      "Hold 3 x 3 hour final review sessions to assist students with final preparations",
-      "Grade variety of quizzes, finals, programming/laboratory assignments encouraging better code quality",
+      "Led a Data Visualization workshop for ACM's Data Science Series",
+      "Worked with 10-15 attendees teaching dataset connections, dashboard development, interactive filters, and a mini project",
+      "Prepared a walkthrough project for attendees to follow along.",
+      "Structured a digestible presentation for even our most novice attendees",
     ],
   },
   {
-    logo: CodeNinjas,
-    group: "Code Ninjas Fremont Hub Branch",
-    location: "Fremont, California",
-    role: "Code Sensei #2",
-    start: "June 2021",
-    end: "September 2021",
-    alt: "Code Ninjas Logo",
+    logo: UCR,
+    group: "CNAS Learning Communities",
+    location: "Riverside, California",
+    role: "Peer Academic Leader",
+    start: "August 2022",
+    end: "January 2023",
+    alt: "UCR Logo",
     description: [
-      "Host summer camps including Roblox Studio, Minecraft Modifications, 3D Printing, Python/Scratch programming",
-      "Lead Create, Junior, After School programs and tours for over 30+ students",
-      "Improved corporate curriculum by creating 50+ supplementary Javascript practice sets with solutions",
-      "Encouraged incorporation of various resources & teaching practices to instructor team to enhance student learning",
-      "Initiated Tik Tok account to increase social presence and increase follower count from 0 to 200+ and 8K+ likes",
+      "Oversaw staff interactions with approximately 1500 first-year students and collaborated with 24 mentors to assist STEM learning for first-years",
+      "Led transition of the program from remote to in-person environment while increasing program enrollment by 20%",
+      " Directed 2 weekly discussions to teach work life balance, develop career plans, monitor academic progress, and provide rationale for university policy and procedures",
     ],
   },
-  {
-    logo: CaliforniaRobotics,
-    group: "Youth Education and Development Group",
-    location: "Fremont, California",
-    role: "Founder / Lead Instructor",
-    start: "August 2019",
-    end: "September 2021",
-    alt: "California Robotics Academy Logo",
-    description: [
-      "Develop 12+ Python, Java, Arduino, TinkerCad curriculums with example demos and train 20+ instructors",
-      "Supervise 28+ weekly camps covering topics in embedded systems, data structures in Python and Java, 3D modeling",
-      "Provide 200+ 2nd - 12th graders with a safe and inspiring learning environment with 65+ positive reviews",
-      "Organize charity events ($600+ donations to Beirut Red Cross, CA Wildfires, Abode Homeless Shelter)",
-      "Schedule, supervise, promote, advertise classes through social media, person-to-person communication",
-    ],
-  },
-  {
-    logo: FirstLegoLeague,
-    group: "Innovant Bots # 46674 & The Athenians # 45077 teams",
-    location: "Fremont, California",
-    role: "Student Coach",
-    start: "August 2019",
-    end: "September 2021",
-    alt: "First Lego League Logo",
-    description: [
-      "Introduce LEGO robotic mechanisms, sensors, programming data structures, odometry",
-      "Host 12+ online workshops covering Scratch programming and usage of ultrasonic and IR sensors",
-      "Co-host 2 outreach events to educate community about First Lego League and energy sustainability projects",
-      "Work alongside 15+ middle schoolers to develop/further their interest in robotics and STEM",
-    ],
-  },
+
 ];
 
 const Teaching = () => {
